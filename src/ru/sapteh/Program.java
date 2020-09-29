15cf0a7bda5374be58e6397b6539a48f95feeed3
ALexey Channov <zolotiekypola5@mail.ru>
2020-09-29 10:28:22 +0300

    create branch student

--- src/ru/sapteh/Program.java
+++ src/ru/sapteh/Program.java
@@ -2,7 +2,7 @@ package ru.sapteh;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello World Hello Java");
+        System.out.println("Hello Bizi");
     System.out.println("Hello Maksim");
     }
 }
