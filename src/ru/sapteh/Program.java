79ca3e0e6083b2016c6f1411a229c06d4bdadd6a
zolotiekypola <72064034+zolotiekypola@users.noreply.github.com>
2020-09-29 10:20:57 +0300

    Update Program.java
    change sout

--- src/ru/sapteh/Program.java
+++ src/ru/sapteh/Program.java
@@ -3,6 +3,6 @@ package ru.sapteh;
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello World Hello Java");
-    System.out.println("Hello Java");
+    System.out.println("Hello Maksim");
     }
 }
