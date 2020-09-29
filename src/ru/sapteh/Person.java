580a73401a45c1a4a945acffc3a4e1881a2f46c0
ALexey Channov <zolotiekypola5@mail.ru>
2020-09-29 11:55:22 +0300

    get and set

--- src/ru/sapteh/Person.java
+++ src/ru/sapteh/Person.java
@@ -3,20 +3,44 @@ package ru.sapteh;
 public class Person {
     private String name;
     private int age;
+    private int date;
+
+    public Person (String name, int age, int date){
+        this.name = name;
+        this.age = age;
+        this.date = date;
+    }
+    public Person(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+    public Person(String name){
+        this.name = name;
+    }
+    public Person (){};
 
     public String getName(){
         return name;
     }
-
     public int getAge() {
         return age;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public void setAge(int age) {
         this.age = age;
+    }
+    public int getDate() {
+        return date;
+    }
+    public void setDate(int date) {
+        this.date = date;
+    }
+    @Override
+    public String toString (){
+       return  "Name: " + getName() + "\n" +
+        "Age: " + getAge() + "\n" +
+        "Date: " + getDate() + "\n";
     }
 }
--- src/ru/sapteh/Program.java
+++ src/ru/sapteh/Program.java
@@ -2,9 +2,12 @@ package ru.sapteh;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello Bizi programist");
-    System.out.println("Hello Maksim clown");
-
+        Person person = new Person("Ivan",15,1111);
+        Person person1 = new Person("Roma",17,1510);
+        Person person2 = new Person("Max",20,1803);
+        System.out.println(person.toString());
+        System.out.println(person1.toString());
+        System.out.println(person2.toString());
 
     }
 }
