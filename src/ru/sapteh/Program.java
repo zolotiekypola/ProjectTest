1a2e6e35042ac411acdadc42ecfa72b849570ad5
ALexey Channov <zolotiekypola5@mail.ru>
2020-09-29 10:13:50 +0300

    Modified Program.java

--- src/ru/sapteh/Program.java
+++ src/ru/sapteh/Program.java
@@ -1,6 +1,7 @@
 package ru.sapteh;
 
 public class Program {
-    public static void main(String[] args) {girt
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 }
