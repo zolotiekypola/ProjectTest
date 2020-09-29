f9fdc5d954ee18a4ab856e9e2c69671e9f3a3e1c
ALexey Channov <zolotiekypola5@mail.ru>
2020-09-29 10:17:58 +0300

    change Program.java

--- src/ru/sapteh/Program.java
+++ src/ru/sapteh/Program.java
@@ -2,10 +2,7 @@ package ru.sapteh;
 
 public class Program {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello World");
-=======
+        System.out.println("Hello World Hello Java");
     System.out.println("Hello Java");
->>>>>>> e0777599f8559d9c4253bffe20229d6f97ba9cae
     }
 }
